e69c451a65d25bdbcaa0a59c7effa8b78773add6
Zack Michener <z@zjm.me>
2025-11-28 12:15:42 -0800

    fix allocator

--- inc/mem.h
+++ inc/mem.h
@@ -1,4 +1,6 @@
 #pragma once
 
+#define Align(n, m)       ((n) == 0 ? 0 : ((((n) - 1) / (m) + 1) * (m)))
+
 void *Alloc(i32 size);
 void Free(void *ptr);
--- src/mem.c
+++ src/mem.c
@@ -1,51 +1,71 @@
 #include "mem.h"
 
+/*
+The heap is divided into allocated blocks. Each block begins with a 32-bit header. If the header is
+positive, the block is allocated; if negative, the block is free. The absolute value of the header
+is the block size in words (not including the header).
+
+Allocation happens by first seeking to a free block, then gathering contiguous free blocks until
+enough space is claimed. The last free block (if not completely claimed) is split, and the claimed
+blocks are marked as one allocated block.
+*/
+
 extern i32 __HEAP_START__[];
 extern i32 __HEAP_END__[];
 
-static void *Seek(i32 *cur, i32 blockSize, i32 size);
-static void *Gather(i32 *cur, i32 blockSize, i32 size);
-
-static void *Seek(i32 *cur, i32 blockSize, i32 size)
-{
-  do {
-    cur += blockSize;
-    blockSize = *cur++;
-  } while (blockSize >= 0 && cur < __HEAP_END__);
-  if (cur == __HEAP_END__) return 0;
-
-  return Gather(cur, blockSize, size);
-}
-
-static void *Gather(i32 *cur, i32 blockSize, i32 size)
+static void Gather(i32 *cur)
 {
   i32 *start = cur;
-  i32 freeSize = -blockSize;
-  while (freeSize < size && cur < __HEAP_END__) {
-    cur += freeSize;
-    blockSize = *cur++;
-    if (blockSize >= 0) {
-      return Seek(cur, blockSize, size);
-    }
-  }
-  if (cur == __HEAP_END__) return 0;
+  i32 blockSize = cur[-1];
+  i32 freeSize = 0;
 
-  start[-1] = size;
-  if (freeSize > size) {
-    start[size] = size - freeSize;
+  while (blockSize <= 0) {
+    freeSize += -blockSize + 1;
+    cur += -blockSize;
+    if (cur >= __HEAP_END__) break;
+    blockSize = *cur++;
   }
-  return start;
+
+  start[-1] = -(freeSize - 1);
+}
+
+static void *Seek(i32 *cur, i32 size)
+{
+  while (cur < __HEAP_END__ && cur[-1] > 0) {
+    cur += cur[-1] + 1;
+  }
+  if (cur >= __HEAP_END__) return 0;
+
+  Gather(cur);
+
+  if (-cur[-1] < size) {
+    cur += -cur[-1] + 1;
+    return Seek(cur, size);
+  }
+
+  return cur;
 }
 
 void *Alloc(i32 size)
 {
-  i32 *cur = __HEAP_START__;
-  i32 blockSize = *cur++;
-  return Seek(cur, blockSize, size);
+  i32 *ptr = __HEAP_START__;
+  i32 words = Align(size, 4) >> 2;
+
+  ptr = Seek(ptr, words);
+  if (!ptr) return 0;
+
+  if (-ptr[-1] > words) {
+    ptr[words] = ptr[-1] + words;
+  }
+
+  ptr[-1] = words;
+
+  return ptr;
 }
 
 void Free(void *ptr)
 {
-  u32 *cur = (u32*)ptr;
+  i32 *cur = (i32*)ptr;
   cur[-1] = -cur[-1];
+  Gather(cur);
 }
